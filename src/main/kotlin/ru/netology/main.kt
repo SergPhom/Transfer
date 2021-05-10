3a8fe1fc2d533d4ce11fc70ba66069f01858212d
Sergey Phomichev <group35student@gmail.com>
2021-05-10 19:15:55 +0300

    Corrected Version 2

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,14 +3,14 @@ package ru.netology
 import java.math.BigDecimal
 
 fun transferInterest(amount: BigDecimal): BigDecimal {
-    if(amount < 35.toBigDecimal()){
-        println("Сумма перевода должна быть от 35 рублей.")
-        return 0.toBigDecimal()
-    }else  return amount.multiply(0.0075.toBigDecimal())
+    val interest = amount.multiply(0.0075.toBigDecimal())
+    return if(interest.compareTo(35.toBigDecimal()) <= 0){
+        35.toBigDecimal()
+    }else interest
 }
 fun main() {
     val transferAmount = 107.50
     val interest = transferInterest(transferAmount.toBigDecimal())
-    if(interest !== 0.toBigDecimal()) println("Сумма перевода - $transferAmount рублей. " +
+    println("Сумма перевода - $transferAmount рублей. " +
             String.format("Сумма комиссии - %.2f рублей.", interest))
 }
