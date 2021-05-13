ce1393c6f9b17487fb06feab9ea7dff97d4ae1f3
Sergey Phomichev <group35student@gmail.com>
2021-05-13 18:22:40 +0300

    Modified Version of MoneyTransfer

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -2,15 +2,49 @@ package ru.netology
 
 import java.math.BigDecimal
 
-fun transferInterest(amount: BigDecimal): BigDecimal {
-    val interest = amount.multiply(0.0075.toBigDecimal())
-    return if(interest.compareTo(35.toBigDecimal()) <= 0){
-        35.toBigDecimal()
-    }else interest
+enum class cardType(val nameOfCard: String){
+    MAESTRO("Maestro"),
+    MASTERCARD("Mastercard"),
+    VISA("Visa"),
+    MIR("Мир"),
+    VKPAY("VK Pay")
 }
+fun typeOfCard(type: String?): cardType{
+    val allTypes = cardType.values()
+    for(t in allTypes){
+        if(t.nameOfCard.equals(type)) return t
+    }
+    return cardType.VKPAY
+}
+
+fun transferInterest(
+    typeOfCard: cardType = cardType.VKPAY,
+    previousAmount: BigDecimal = 0.toBigDecimal(),
+    amount: BigDecimal
+): BigDecimal =
+    when(typeOfCard){
+        cardType.MAESTRO, cardType.MASTERCARD -> {
+            if(previousAmount in 300.toBigDecimal()..75000.toBigDecimal()){
+                0.toBigDecimal()
+            }else amount.multiply(0.006.toBigDecimal()) + 20.toBigDecimal()
+        }
+        cardType.MIR, cardType.VISA -> {
+            val visaPercentage =amount.multiply(0.0075.toBigDecimal())
+            if(visaPercentage.compareTo(35.toBigDecimal()) > 0) {
+                visaPercentage
+            }else 35.toBigDecimal()
+        }
+        else -> 0.toBigDecimal()
+    }
+
 fun main() {
     val transferAmount = 107.50
-    val interest = transferInterest(transferAmount.toBigDecimal())
-    val interestOutput = String.format("Сумма комиссии - %.2f рублей.", interest)
+    val previousAmount = 150
+    val cardType = "Maestro"
+    val result = transferInterest(
+        typeOfCard = typeOfCard(cardType),
+        previousAmount = previousAmount.toBigDecimal(),
+        amount = transferAmount.toBigDecimal())
+    val interestOutput = String.format("Сумма комиссии - %.2f рублей.", result)
     println("Сумма перевода - $transferAmount рублей.  $interestOutput")
 }
